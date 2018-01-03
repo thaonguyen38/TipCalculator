94f7211691ec61dfb6dea6252c44f837a40744ed
Thao Nguyen <tnn040@ucsd.edu>
2018-01-02 16:48:23 -0800

    tip cal

---
+++ TipCal/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  TipCal
+//
+//  Created by main_user on 1/2/18.
+//  Copyright © 2018 Thao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        tipLabel.text=String(format: "$%.2f", tip)
+        totalLabel.text=String(format: "$%.2f", total)
+        
+    }
+    
+}
+
